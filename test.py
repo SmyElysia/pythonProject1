6aa83f5cb2a122bb33c295cc38468d3d64245285
SmyElysia <1254099253@qq.com>
2024-07-19 09:54:01 +0800

    1

---
+++ test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import math
+print ("lzynb")
+b=7%4
+print(b)
+a=math.sqrt(100)
+print(a)
+length=len("lzynb")
+print(length)
+print("Py"+"thon")
+name="lzynb"
+newname=name.upper()
+print(newname)
+newname2=newname.replace("LZY","XZM")
+print(newname2)
+print("You're \"Welcome\"")
+print("*\n**\n***")
+first=input("Enter your first name: ")
+print(first)
